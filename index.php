6cfe11864018dc34727148a6722ef2691fe87b04
Andraž Kržišnik <andraz.krzisnik1993@gmail.com>
2025-06-15 14:35:03 +0200

    update

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php get_header(); ?>
 
-<h1 class="text-center">hello world</h1>
-<div class="grid">
-  <div>1</div>
-  <div>2</div>
-  <div>3</div>
-  <div>4</div>
-</div>
 
 <?php get_footer(); ?>
 
